386c47b4eec7a8f25a95040b76ff48539bab05da
Pedro Castro <falecompedroac@gmail.com>
2026-02-07 18:38:44 +0000

    added tailwind

--- astro.config.ts
+++ astro.config.ts
@@ -3,9 +3,14 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
 	site: "https://peucastro.me",
 	integrations: [svelte(), mdx(), sitemap()],
+
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
