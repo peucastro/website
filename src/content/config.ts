c81a3ec9118bfe5ef8f0525559de9e6326f209fd
Pedro Castro <falecompedroac@gmail.com>
2026-02-08 00:18:25 +0000

    content config

---
+++ src/content/config.ts
@@ -0,0 +1,39 @@
+import { defineCollection, z } from "astro:content";
+
+const blog = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string().min(5, "Title is too short").max(60, "Title is too long"),
+    description: z.string().min(20, "Description is too short").max(160, "Description is too long"),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    tags: z
+      .array(z.string())
+      .refine((items) => new Set(items).size === items.length, {
+        message: "tags must be unique",
+      })
+      .optional(),
+  }),
+});
+
+const projects = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string().max(200, "Description is too long"),
+    pubDate: z.coerce.date(),
+    tags: z
+      .array(z.string())
+      .refine((items) => new Set(items).size === items.length, {
+        message: "tags must be unique",
+      })
+      .optional(),
+    image: z.string(),
+    liveUrl: z.string().url().optional(),
+    repositoryUrl: z.string().url().optional(),
+    status: z.enum(["active", "completed", "archived"]).default("completed"),
+    technologies: z.array(z.string()).optional(),
+  }),
+});
+
+export const collections = { blog, projects };
